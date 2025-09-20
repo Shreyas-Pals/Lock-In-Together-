639786769c2775768642c05b23d039c017341aa7
Shreyas-Pals <shreyas.2007.oct@gmail.com>
2025-09-20 10:45:43 +0530

    Added Prompts for User, Stored user data, Messages and message types added, Working on Spotify Auth

--- CLI/client.py
+++ CLI/client.py
@@ -1,15 +1,16 @@
 import socket
+import threading
 
 HEADER = 64
 PORT = 5050
 FORMAT = 'utf-8'
 DISCONNECT_MSG = '^DISCONNECT!'
-SERVER = '169.254.146.2'
+SERVER = '172.16.140.82'
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 client.connect((SERVER,PORT))
-
+master_role =  False
 def send(msg):
     message = msg.encode(FORMAT)
     msg_length = len(message)
@@ -20,6 +21,42 @@ def send(msg):
     client.send(send_length)
     client.send(message)
 
-send('Hello World!')
+def listen():
+    while True:
+        msg_length = client.recv(HEADER).decode(FORMAT)
+        if msg_length:
+            msg_length = int(msg_length)
+            msg = client.recv(msg_length).decode(FORMAT)
+            split_ = msg.split("|")
+            if len(split_)>2:
+                type,message = split_[1], split_[2]
+        
+        '''
+        MESSAGE TYPES:-
+        '''
 
-send(DISCONNECT_MSG)
+        if msg.startswith('NOTICE|'):
+            print(f'[{split_[0]}] {message}')
+            if type == 'ROLE':
+                global master_role
+                master_role = True
+                send('CONFIRM|ROLE')
+
+        elif msg.startswith('PROMPT|'):
+            name = input(f'[{split_[0]}]{message} ')
+
+            if type == 'NAME':
+                send(f"RESPONSE|{type}|{name}")
+
+        # send(DISCONNECT_MSG)
+        else:
+            print(f'[SERVER]:{msg}')
+thread = threading.Thread(target=listen)
+thread.start()
+thread.join()
+# client.close()
+
+
+    # send('Hello World!')
+
+# send(DISCONNECT_MSG)
--- CLI/server.py
+++ CLI/server.py
@@ -6,6 +6,7 @@ HOST = socket.gethostbyname(socket.gethostname())
 PORT = 5050  
 FORMAT = 'utf-8'
 DISCONNECT_MSG = '^DISCONNECT!'
+ASSIGN_MASTER_MSG = 'ROLE|MASTER'
 
 print(f"Server running on {HOST}:{PORT}")
 
@@ -13,8 +14,26 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
 server.bind((HOST, PORT))
 
+# List of all clients' infos
+clients = []
+# Info belonging to a single client
+client_info = {}
+master_event = threading.Event()  
+
+
+def send_msg(conn,msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' '*(HEADER - len(send_length))
+
+    conn.send(send_length)
+    conn.send(message)
+
+
+
 def handle_client(conn,addr):
-    print('New connection {addr} connected ')
+    print(f'New connection {addr} connected ')
     connected = True
     while connected:
         msg_length = conn.recv(HEADER).decode(FORMAT)
@@ -22,7 +41,19 @@ def handle_client(conn,addr):
             msg_length = int(msg_length)
             msg = conn.recv(msg_length).decode(FORMAT)
             if msg == DISCONNECT_MSG:
+                if conn == clients[0]['conn']:
+                    master_event.clear()
+                clients.remove(client_info)
                 connected = False
+                
+            if msg.startswith('RESPONSE|'):
+                response = msg.split("|")[2]
+                client_info['name'] = response
+                send_msg(conn,f'Hello {response} - Get ready to lock-in!')
+            
+            if msg.startswith('CONFIRM|'):
+                master_event.set()
+                
             print(f'[{addr} sent {msg}]')
         
     conn.close()
@@ -31,9 +62,20 @@ def start():
     server.listen()
     while True:
         conn,addr = server.accept()
+        client_info['conn'] = conn
+
         thread = threading.Thread(target=handle_client, args = (conn,addr))
         thread.start()
-        print(f'Active connections:{threading.active_count()-1}')
 
-print('Server is starting')
-start()
+        clients.append(client_info)
+        if not master_event.is_set(): 
+            first_client = clients[0]['conn']
+            send_msg(first_client,'NOTICE|ROLE|You are the Admin.')
+
+        send_msg(conn,'PROMPT|NAME| Enter your username:')
+        # print(f'Active connections:{threading.active_count()-1}')
+
+# print('Server is starting')
+
+if __name__ == "__main__":
+    start()
